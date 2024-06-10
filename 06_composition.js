06fe004e894677212c6323b4017daf2aba53683a
Ouerdani Sakina <ouerdanisakina@gmail.com>
2024-06-10 15:12:18 +0200

    exercice6

--- 06_composition.js
+++ 06_composition.js
@@ -7,8 +7,10 @@
  *  - ne pas utiliser la méthode concat, map, merge, push
  *  - for, foreach, while, do while sont interdits
  */
-
-const concat = (arr1, arr2) => {}
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const concat = (arr1, arr2) => [...arr1, ...arr2];
+console.log(concat(arr1, arr2));
 
 /**
  * utiliser l'opérateur de composition ... afin d'ajouter un élément à un tableau
@@ -19,15 +21,20 @@ const concat = (arr1, arr2) => {}
  * - ne pas utiliser la méthode push
  */
 
-const push = (arr, item) => {}
+const tab = [1, 2, 3];
+const a = 4
+const push = (arr, item) => [...arr, item];
+console.log(push(tab, a));
 
 /**
  * utiliser l'opérateur de composition ... afin de fusionner 2 objets passés en paramètres
  * 
  * ex: {a: 1, b: 2}, {c: 3, d: 4} => {a: 1, b: 2, c: 3, d: 4}
  */
-
-const merge = (obj1, obj2) => {}
+const obj1 = {a:1, b:2};
+const obj2 = {c:3, d:4}
+const merge = (obj1, obj2) => ({...obj1, ...obj2});
+console.log(merge(obj1, obj2));
 
 /**
  * utiliser l'opérateur de composition ... afin de modifier la propriété name de l'objet
@@ -38,7 +45,8 @@ const merge = (obj1, obj2) => {}
  *  - interdiction d'utiliser l'opérateur d'affectation "="
  */
 
-const setName = (obj, name) => {}
-
+const obj = {name: 'toto'};
+const setName = (obj, name) => ({...obj, name});
+console.log(setName(obj, 'titi'));
 // astuce: {...obj} crée une copie de l'objet, c'est un des principes de l'immutabilité et évite les problèmes de référence
 module.exports = {concat, push, merge, setName}
